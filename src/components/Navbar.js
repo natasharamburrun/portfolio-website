2eee3caff1af536272d1127b4087be9e1354442e
Natasha Ramburrun <r_natasha100@hotmail.com>
2022-05-26 22:36:49 +0100

    styled the tablet and desktop header

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -25,19 +25,19 @@ const Navbar = () => {
             </button>
           }
          </div>
-      </div>
-      <div className={navbarOpen ? "nav-item active" : " nav-item"}>
-        <ul className='menu'>
-          <li className='menu-item'>
-            <Link to="/">Home</Link>
-          </li>
-          <li className='menu-item'>
-            <Link to="/portfolio">Portfolio</Link>
-          </li>
-          <li className='menu-item'>
-            <Link to="/contact-me">Contact Me</Link>
-          </li>
-        </ul>
+        <div className={navbarOpen ? "nav-item active" : " nav-item"}>
+          <ul className='menu'>
+            <li className='menu-item'>
+              <Link to="/">Home</Link>
+            </li>
+            <li className='menu-item'>
+              <Link to="/portfolio">Portfolio</Link>
+            </li>
+            <li className='menu-item'>
+              <Link to="/contact-me">Contact Me</Link>
+            </li>
+          </ul>
+        </div>
       </div>
     </nav>
   );
