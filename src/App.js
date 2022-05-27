fbc67d69adff4693fa76103ed29e8c9448249091
Natasha Ramburrun <r_natasha100@hotmail.com>
2022-05-27 15:17:47 +0100

    Removed unessasary js files, added footer and styled for mobile viewpoint

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar"
 import Homepage from './components/Homepage';
+import Footer from './components/Footer';
 import './styles/styles.scss';
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
         {/* <Route path='/portfolio' element={Portfolio} />
         <Route path='/contact-me' element={Contact-Me} /> */}
       </Routes>
+      <Footer />
     </Router>
   );
 }
--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,5 +1,4 @@
 import React from "react";
-import Footer from "./Footer";
 import mobileHero from "../assets/images/homepage/mobile/image-homepage-hero.jpg";
 import mobileHero2 from "../assets/images/homepage/mobile/image-homepage-hero@2x.jpg";
 import tabletHero from "../assets/images/homepage/tablet/image-homepage-hero.jpg";
@@ -61,7 +60,6 @@ function Homepage() {
         <hr />
         <button className="button secondary-button">Contact me</button>
       </article>
-      <Footer />
     </section>
   );
 }
